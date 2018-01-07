8ada7f3c045df23051df218391dfd3ee5d446b8b
Georgi Kostov <gkostov@vmware.com>
2018-01-07 19:13:14 +0200

    dropped wrong code snippet

--- algorithms/include/sort.hpp
+++ algorithms/include/sort.hpp
@@ -16,17 +16,4 @@ void insertion_sort(std::vector<T>& a)
 	}
 }
 
-template <typename RandomIt, class <RandomIt> Range<RandomIt, RandomIt> >
-void insertion_sort2(std::vector<T>& a)
-{
-	std::size_t n = a.size();
-	for (std::size_t i = 1; i < n; ++i)
-	{
-		for (std::size_t j = i; j > 0 && a[j] < a[j-1]; --j)
-		{
-			std::swap(a[j-1], a[j]);
-		}
-	}
-}
-
 #endif // SORT_H
